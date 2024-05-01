869ae313434b75cc085c6ff9962993ddcd4f5cf7
TejasMaitri <110625203+TejasMaitri@users.noreply.github.com>
2024-05-01 20:31:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,8 +102,7 @@ def evaluate_forecast(actual, forecast):
 def download_csv(data):
     csv_file = data.to_csv(index=False)
     b64 = base64.b64encode(csv_file.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{
-        b64}" download="forecasted_data.csv">Download Forecasted Data as CSV</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="forecasted_data.csv">Download Forecasted Data as CSV</a>'
     st.markdown(href, unsafe_allow_html=True)
 
 # Main function
